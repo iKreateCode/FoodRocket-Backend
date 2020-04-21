5bc75b8a3c130a0ac36075649971670dfa278e60
Waleed <waleedi825@gmail.com>
2020-04-21 22:09:47 +0100

    Order migration & model implemented

---
+++ database/migrations/2020_04_21_202820_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_type');
+            $table->double('total');
+            $table->text('note');
+            $table->unsignedBigInteger('payment_id');
+            $table->unsignedBigInteger('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
