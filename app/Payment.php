d791a434ad4a0d8378a5ee296fbcefeea92ef656
Waleed <waleedi825@gmail.com>
2020-04-22 20:52:24 +0100

    Payment model & migration implemented

---
+++ app/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id', 'payment_method_id', 'amount_paid'
+    ];
+}
